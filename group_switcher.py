a1d00d794acee8d5d0a8ab7516caee4c5699db35
RUSTEMATOR <samoilenkorustem@gmail.com>
2024-09-20 10:40:54 +0300

    a

---
+++ group_switcher.py
@@ -0,0 +1,44 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+class TestData:
+    emails = []
+
+
+
+def group_switcher(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False,
+                                         proxy={
+                                             'server': 'http://138.197.150.103:8090',
+                                             'username': 'kbc',
+                                             'password': '347SP&Uwqt!2xZ7w',
+                                         })
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://frequency.administrativedistrict.com/login")
+    page.get_by_placeholder("Email...").click()
+    page.get_by_placeholder("Email...").fill("ross@kingbilly.com")
+    page.get_by_placeholder("Пароль").fill("li@XO?ijQxK6")
+    page.get_by_role("button", name="Вход").click()
+
+    for email in TestData.emails:
+        page.get_by_role("textbox", name="Search").click()
+        page.get_by_role("textbox", name="Search").fill(email)
+        page.get_by_role("button", name="search").click()
+        page.locator(
+            'xpath=/html/body/div[5]/div[2]/div[2]/div/div/div/div/div/div[2]/table/tbody/tr/td[3]/a/span').first.click()
+        page.get_by_role("button", name="Player").click()
+        page.get_by_role("listbox").get_by_role("option", name="Test player").click()
+        page.locator("#dataForm").get_by_role("button", name="Save").click()
+        page.get_by_role("button", name="OK").click()
+        page.get_by_role("link", name=" Players").click()
+        print(f'{email} "is marked as test acc"')
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+if __name__ == "__main__":
+    with sync_playwright() as playwright:
+        group_switcher(playwright)
---
+++ regular_expression.py
@@ -0,0 +1,25 @@
+import re
+
+
+with open('mash_of_info.txt', 'r') as file:
+    mash_of_info = file.read()
+
+
+
+
+
+# Regular expression to find emails and replace '_' with '@' before 'kingbilly.xyz'
+email_pattern = r"([^\s@]+)_kingbilly\.xyz"
+replacement = r"\1@kingbilly.xyz"
+
+# Extract and replace to format as emails
+extracted_emails = re.sub(email_pattern, replacement, mash_of_info)
+
+# Further refine to only keep the emails, removing all other text
+only_emails = re.findall(r"[^\s@]+@kingbilly\.xyz", extracted_emails)
+
+# Join the emails into a single string separated by newlines
+cleaned_emails = "\n".join(only_emails)
+
+print(cleaned_emails)
+
